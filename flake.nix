094c0f6ca824de55a1f89f49f3db13aba54a019f
Nikolay Petrov <sungon@gmail.com>
2024-10-20 19:53:55 -0400

    add nix flake/direnv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "kleverr is a library for showing stacktraces in errors";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+      });
+}
